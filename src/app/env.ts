2b463f44d1312e26bdf1d2217c3a8e361a3a7db3
HenryRuiz332 <henryruiz332@gmail.com>
2023-01-10 21:52:36 -0500

    add images to firebase

---
+++ src/app/env.ts
@@ -0,0 +1,6 @@
+export class env {
+ 
+    public static appUrlProduction: string = "https://henryruiz332.github.io/henry-portfolio.github.io"
+    public static appUrlLocal: string = "http://localhost:4200"
+
+}
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -15,7 +15,7 @@ export class ProjectsComponent {
 
   ngOnInit(): void {
     this.projects = projects
-    console.log(this.projects)
+    
     
   }
 
